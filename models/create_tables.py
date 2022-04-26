c3edaffa9ec3fa2e678672855cabee32827637c3
alphacpc <alphacpc@gmail.com>
2022-04-26 19:24:40 +0000

    Amelioration de la creation des tables

--- models/create_tables.py
+++ models/create_tables.py
@@ -1,92 +1,155 @@
-from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-# configurations syst
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://groupe3:passer123@localhost/projet_flask'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+    
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://groupe3:passer123@localhost/projet_flask'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
+db = SQLAlchemy(app)
 
-# creating database connection
-mydb=SQLAlchemy(app)
 
-class adresse(mydb.Model):
-    __tablename__='adresse'
-    users_id = relationship("users", backref="adresse")
-    id_addresse = mydb.Column(mydb.Integer, primary_key=True)
-    street = mydb.Column(mydb.String(200))
-    suite = mydb.Column(mydb.String(200))
-    city = mydb.Column(mydb.String(200))
-    zipcode = mydb.Column(mydb.String(100))
-    lat = mydb.Column(mydb.String(50))
-    long = mydb.Column(mydb.String(50))
+###############################
+######CREATION DES TABLES######
+###############################
 
-class company(mydb.Model):
-    users = relationship("users", backref="company")
-    id_company = mydb.Column(mydb.Integer, primary_key=True)
-    name = mydb.Column(mydb.String(200))
-    catchPhrase = mydb.Column(mydb.String(250))
-    bs = mydb.Column(mydb.String(200))
-class users(mydb.Model):
-    __tablename__='users'
-    id_users = mydb.Column(mydb.Integer, primary_key=True)
-    userName = mydb.Column(mydb.String(200))
-    Email = mydb.Column(mydb.String(200))
-    phone = mydb.Column(mydb.String(50))
-    website = mydb.Column(mydb.String(200))
+
+####TAB COMPAGNY
+class Adresses(db.Model):
+    __tablename__ = 'adresses'
+    
+    id_addresse = db.Column(db.Integer, primary_key = True)
+    street = db.Column(db.String(250), nullable = False)
+    suite = db.Column(db.String(200), nullable = False)
+    city = db.Column(db.String(200), nullable = False)
+    zipcode = db.Column(db.String(100), nullable = False)
+    lat = db.Column(db.String(50), nullable = False)
+    long = db.Column(db.String(50), nullable = False)
+
+    # Adresses childreen relations
+    users = relationship("users", backref = "adresse")
+
+    def __ref__(self):
+        return '<Adresses %r>' % self.city
+
+
+
+####TAB COMPAGNY
+class Compagny(db.Model):
+    __tablename__ = 'company'
+
+    id_company = db.Column(db.Integer, primary_key = True)
+    name_compagny = db.Column(db.String(150), nullable = False, unique = True)
+    catchPhrase = db.Column(db.String(150), nullable = True)
+    bs = db.Column(db.String(150), nullable = False)
+
+    # Compagny childreen relations
+    users = relationship("users", backref = "company")
+
+    def __ref__(self):
+        return '<Compagny %r>' % self.name_compagny
+
+
+
+####TAB USERS
+class Users(db.Model):
+    __tablename__ = 'users'
+    id_users = db.Column(db.Integer, primary_key = True)
+    fullname = db.Column(db.String(100), nullable = False)
+    username = db.Column(db.String(50), nullable = False, unique = True)
+    email = db.Column(db.String(50), nullable = False, unique = True)
+    phone = db.Column(db.String(50), nullable = True)
+    website = db.Column(db.String(100), nullable = True)
 
     # users childreen relations
-    posts = relationship("posts", backref="users")
-    todos = relationship("todos", backref="users")
-    albums = relationship("albums", backref="users")
+    posts = relationship("posts", backref = "users")
+    todos = relationship("todos", backref = "users")
+    albums = relationship("albums", backref = "users")
     
     # users parents relations
-    id_adresse = mydb.Column(mydb.Integer, mydb.ForeignKey('adresse.id_adresse'))
-    id_company = mydb.Column(mydb.Integer, mydb.ForeignKey('company.id_company'))
+    id_adresse_users = db.Column(db.Integer, db.ForeignKey('adresse.id_adresse'))
+    id_company_users = db.Column(db.Integer, db.ForeignKey('company.id_company'))
     
-class posts(mydb.Model):
-    __tablename__='posts'
-    id_posts = mydb.Column(mydb.Integer, primary_key=True)
-    title_posts = mydb.Column(mydb.String(200))
-    body = mydb.Column(mydb.String(200))
+    def __repr__(self):
+        return '<Users %r>' % self.fullname
 
-    comments = relationship("comments")
 
-    id_users = mydb.Column(mydb.Integer, mydb.ForeignKey('users.id_users'))
 
-class comments(mydb.Model):
-    __tablename__='comments'
-    id_comments = mydb.Column(mydb.Integer, primary_key=True)
-    name = mydb.Column(mydb.String(200))
-    Email = mydb.Column(mydb.String(100))
-    body = mydb.Column(mydb.String(200))
+####TAB POSTS
+class Posts(db.Model):
+    __tablename__ = 'posts'
+    id_posts = db.Column(db.Integer, primary_key = True)
+    title_posts = db.Column(db.String(150), nullable = False)
+    body_posts = db.Column(db.Text, nullable = False)
+    id_users_posts = db.Column(db.Integer, db.ForeignKey('users.id_users'))
 
-    id_posts = mydb.Column(mydb.Integer, mydb.ForeignKey('posts.id_posts'))
+    # users childreen relations
+    comments = relationship("Comments", backref='posts')
 
-class todos(mydb.Model):
-    __tablename__='todos'
-    id_todos = mydb.Column(mydb.Integer, primary_key=True)
-    title_todos= mydb.Column(mydb.String(200))
+    def __repr__(self):
+        return '<Posts %r>' % self.title_posts
 
-    id_users = mydb.Column(mydb.Integer, mydb.ForeignKey('users.id_users'))
 
-class albums(mydb.Model):
-    __tablename__='albums'
-    id_albums = mydb.Column(mydb.Integer, primary_key=True)
-    title_albums= mydb.Column(mydb.String(200))
 
-    photos= relationship("photos")
 
-class photos(mydb.Model):
-    __tablename__="photos"
-    id_photos = mydb.Column(mydb.Integer, primary_key=True)
-    albums = mydb.Column(mydb.String(200))
-    title_photos= mydb.Column(mydb.String(200))
-    url = mydb.Column(mydb.String(200))
-    thambnailUrl= mydb.Column(mydb.String(200))
+####TAB POSTS
+class Comments(db.Model):
+    __tablename__ = 'comments'
+    id_comments = db.Column(db.Integer, primary_key = True)
+    name_comments = db.Column(db.String(150), nullable = False)
+    email_comments = db.Column(db.String(100), nullable = False)
+    body_comments = db.Column(db.Text, nullable = False)
+    id_posts_comments = db.Column(db.Integer, db.ForeignKey('posts.id_posts'))
 
-    id_albums = mydb.Column(mydb.Integer, mydb.ForeignKey('albums.id_albums'))
+    def __repr__(self):
+        return '<Comments %r>' % self.name_comments
+
+
+####TAB TODOS
+class Todos(db.Model):
+    __tablename__ = 'todos'
+    id_todos = db.Column(db.Integer, primary_key = True)
+    title_todos = db.Column(db.String(100), nullable = False)
+    status = db.Column(db.Integer, nullable = False)
+    id_users_todos = db.Column(db.Integer, db.ForeignKey('users.id_users'))
+
+    def __repr__(self):
+        return '<Todos %r>' % self.title_todos
+
+
+
+####TAB ALBUMS
+class Albums(db.Model):
+    __tablename__ = 'albums'
+    id_albums = db.Column(db.Integer, primary_key = True)
+    title_albums = db.Column(db.String(150), nullable=False)
+    id_users_albums = db.Column(db.Integer, db.ForeignKey('users.id_users'))
+
+    photos = relationship('Photos', backref ='albums')
+
+    def __repr__(self):
+        return '<Albums %r>' % self.title_albums
+
+
+
+
+####TAB PHOTOS
+class Photos(db.Model):
+    __tablename__ = "photos"
+    id_photos = db.Column(db.Integer, primary_key = True)
+    title_photos = db.Column(db.String(150), nullable=False)
+    url = db.Column(db.String(150), nullable=False)
+    thambnailUrl = db.Column(db.String(150), nullable=False)
+    id_albums_photos = db.Column(db.Integer, db.ForeignKey('albums.id_albums'))
+
+
+    def __repr__(self):
+        return '<Photos %r>' % self.title_photos
+
+
 
 
 if __name__=='__main__':
-    mydb.create_all()
+    db.create_all()
