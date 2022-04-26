bdc920b111f133c9bd84588afe5713a825b1796a
alphacpc <alphacpc@gmail.com>
2022-04-26 09:37:09 +0000

    added create_tables on models

--- models/create_tables.py
+++ models/create_tables.py
@@ -9,32 +9,41 @@ app.config['SQLALCHEMY_DATABASE_URI']='postgresql://groupe3:passer123@localhost/
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
 # creating database connection
-mydb=SQLAlchemy(app)
+db = SQLAlchemy(app)
 
-class adresse(mydb.Model):
+class adresse(db.Model):
     __tablename__='adresse'
     users_id = relationship("users", backref="adresse")
-    id_addresse = mydb.Column(mydb.Integer, primary_key=True)
-    street = mydb.Column(mydb.String(200), nullable=False)
-    suite = mydb.column(mydb.String(200), nullable=False)
-    city = mydb.Column(mydb.String(200), nullable=False)
-    zipcode = mydb.Column(mydb.String(100), nullable=False)
-    lat = mydb.Column(mydb.String(50), nullable=False)
-    long = mydb.Column(mydb.String(50), nullable=False)
+    id_addresse = db.Column(db.Integer, primary_key=True)
+    street = db.Column(db.String(200), nullable=False)
+    suite = db.column(db.String(200), nullable=False)
+    city = db.Column(db.String(200), nullable=False)
+    zipcode = db.Column(db.String(100), nullable=False)
+    lat = db.Column(db.String(50), nullable=False)
+    long = db.Column(db.String(50), nullable=False)
 
-class company(mydb.Model):
+
+
+
+
+class company(db.Model):
     users = relationship("users", backref="company")
-    id_company = mydb.Column(mydb.Integer, primary_key=True)
-    name = mydb.Column(mydb.String(200), nullable=False)
-    catchPhrase = mydb.column(mydb.String(200), nullable=False)
-    bs = mydb.Column(mydb.String(200), nullable=False)
-class users(mydb.Model):
+    id_company = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    catchPhrase = db.column(db.String(200), nullable=False)
+    bs = db.Column(db.String(200), nullable=False)
+
+
+
+
+
+class users(db.Model):
     __tablename__='users'
-    id_users = mydb.Column(mydb.Integer, primary_key=True)
-    userName = mydb.Column(mydb.String(200), nullable=False)
-    Email = mydb.column(mydb.String(200), nullable=False)
-    phone = mydb.Column(mydb.String(50), nullable=False)
-    website = mydb.Column(mydb.String(200), nullable=False)
+    id_users = db.Column(db.Integer, primary_key=True)
+    userName = db.Column(db.String(200), nullable=False)
+    Email = db.column(db.String(200), nullable=False)
+    phone = db.Column(db.String(50), nullable=False)
+    website = db.Column(db.String(200), nullable=False)
 
     # users childreen relations
     posts = relationship("posts", backref="users")
@@ -42,48 +51,64 @@ class users(mydb.Model):
     albums = relationship("albums", backref="users")
     
     # users parents relations
-    id_adresse = mydb.column(mydb.Integer, mydb.ForeignKey('adresse.id_adresse'))
-    id_company = mydb.column(mydb.Integer, mydb.ForeignKey('company.id_company'))
+    id_adresse = db.column(db.Integer, db.ForeignKey('adresse.id_adresse'))
+    id_company = db.column(db.Integer, db.ForeignKey('company.id_company'))
+
+
+
+
     
-class posts(mydb.Model):
+class posts(db.Model):
     __tablename__='posts'
-    id_posts = mydb.Column(mydb.Integer, primary_key=True)
-    title_posts = mydb.Column(mydb.String(200), nullable=False)
-    body = mydb.column(mydb.String(200), nullable=False)
+    id_posts = db.Column(db.Integer, primary_key=True)
+    title_posts = db.Column(db.String(200), nullable=False)
+    body = db.column(db.String(200), nullable=False)
 
     comments = relationship("comments")
 
-    id_users = mydb.column(mydb.Integer, mydb.ForeignKey('users.id_users'))
+    id_users = db.column(db.Integer, db.ForeignKey('users.id_users'))
 
-class comments(mydb.Models):
+
+
+
+class comments(db.Models):
     __tablename__='comments'
-    id_comments = mydb.Column(mydb.Integer, primary_key=True)
-    name = mydb.Column(mydb.String(200), nullable=False)
-    Email = mydb.column(mydb.String(100), nullable=False)
-    body = mydb.column(mydb.String(200), nullable=False)
+    id_comments = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    Email = db.column(db.String(100), nullable=False)
+    body = db.column(db.String(200), nullable=False)
 
-    id_posts = mydb.column(mydb.Integer, mydb.ForeignKey('posts.id_posts'))
+    id_posts = db.column(db.Integer, db.ForeignKey('posts.id_posts'))
 
-class todos(mydb.Models):
+
+
+
+class todos(db.Models):
     __tablename__='todos'
-    id_todos = mydb.Column(mydb.Integer, primary_key=True)
-    title_todos= mydb.column(mydb.String(200), nullable=False)
+    id_todos = db.Column(db.Integer, primary_key=True)
+    title_todos= db.column(db.String(200), nullable=False)
 
-    id_users = mydb.column(mydb.Integer, mydb.ForeignKey('users.id_users'))
+    id_users = db.column(db.Integer, db.ForeignKey('users.id_users'))
 
-class albums(mydb.Models):
+
+    
+
+class albums(db.Models):
     __tablename__='albums'
-    id_albums = mydb.Column(mydb.Integer, primary_key=True)
-    title_albums= mydb.column(mydb.String(200), nullable=False)
+    id_albums = db.Column(db.Integer, primary_key=True)
+    title_albums= db.column(db.String(200), nullable=False)
 
     photos= relationship("photos")
 
-class photos(mydb.Models):
-    __tablename__="photos"
-    id_photos = mydb.Column(mydb.Integer, primary_key=True)
-    albums = mydb.column(mydb.String(200), nullable=False)
-    title_photos= mydb.column(mydb.String(200), nullable=False)
-    url = mydb.column(mydb.String(200), nullable=False)
-    thambnailUrl= mydb.column(mydb.String(200), nullable=False)
 
-    id_albums = mydb.column(mydb.Integer, mydb.ForeignKey('albums.id_albums'))
+
+
+class photos(db.Models):
+    __tablename__="photos"
+    id_photos = db.Column(db.Integer, primary_key=True)
+    albums = db.column(db.String(200), nullable=False)
+    title_photos= db.column(db.String(200), nullable=False)
+    url = db.column(db.String(200), nullable=False)
+    thambnailUrl= db.column(db.String(200), nullable=False)
+
+    id_albums = db.column(db.Integer, db.ForeignKey('albums.id_albums'))
