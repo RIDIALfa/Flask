b4627e326496e4f2b3a47f5f1c59a4c01b54e119
alphacpc <alphacpc@gmail.com>
2022-05-05 17:46:51 +0000

    remove cascading.js

--- static/javascript/cascading.js
+++
@@ -1,7 +0,0 @@
-// Delete = function(x){
-//     console.log("delete")
-//     x.parentNode.parentNode.parentNode.remove()
-// }
-// var table=document.getElementById("postTable")
-// var table_nb=document.getElementById('tbody').childElementCount
-// console.log(table_nb)
